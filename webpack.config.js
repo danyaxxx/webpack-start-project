891eaeabaf1533ad3f421b1595e7cb867b29b5ec
Daniil <danya.trishchanovskiy.01@mail.ru>
2020-07-02 19:09:00 +0300

    Start Webpack 4 and Babel

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports = {
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+}
